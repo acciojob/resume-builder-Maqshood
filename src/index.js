fdd2e3f5415451b6f2c276037450c12c6b9362cd
SK Maqshood <skmaqshood99@gmail.com>
2025-08-05 16:06:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
